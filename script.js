de7bd94ab26c9612043c47cc9de80e5cf3ce5e87
Mark Batson <markdbatson@gmail.com>
2018-03-27 14:35:31 -0400

    Styling changes and more
    Added style changes for the index and home page
    Added Demo CAS login page
    Added javascript functionality to registration page

---
+++ script.js
@@ -0,0 +1,47 @@
+function enablePrefix() {
+  if (this.value == 'Sp18') {
+    document.getElementById('prefix').disabled = true;
+  }
+  else {
+    document.getElementById('prefix').disabled = false;
+  }
+}
+
+function enableCourse() {
+  console.log(document.getElementById('prefix').value);
+  console.log(document.getElementById('prefix').value == 'CSCI')
+
+  if (document.getElementById('prefix').value == 'CSCI') {
+    document.getElementById('course').disabled = false;
+  }
+  else {
+    document.getElementById('course').disabled = true;
+    document.getElementById('course').value = 'default';
+  }
+}
+
+// Get the modal
+var modal = document.getElementById('detail');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
